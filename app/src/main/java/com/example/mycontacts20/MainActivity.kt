663cdb02e7ed8be69baad8ce71c8ac01dd283486
sipokobridget-sketch <sipokobridget@gmail.com>
2026-03-12 13:25:38 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/mycontacts20/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts20/MainActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val firstName="bridget"
+        val lastName="sipoko"
+        var address="84 on main complex"
+        var isMale=false
+        val dayOfBirth=21
+        val monthOfBirth="march"
+        val yearOfBirth=2007
+        Log.d("hi",firstName +" "+ lastName +"you live in"+ address + "it is" +isMale+ "that you are male"+dayOfBirth+" "+monthOfBirth+" "+yearOfBirth)
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
